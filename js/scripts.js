c0f7c3f0f99748df5db214bf41bd75340deb1daf
firmanzz <124325264+firmanzz@users.noreply.github.com>
2024-05-29 19:08:26 +0700

    header

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,94 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var tombolDetailPaslon1 = document.getElementById("tombolDetailPaslon1");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./detail-calon-1.html";
+        });
+      }
+      
+      var tombolDetailPaslon2 = document.getElementById("tombolDetailPaslon2");
+      if (tombolDetailPaslon2) {
+        tombolDetailPaslon2.addEventListener("click", function (e) {
+          window.location.href = "./detail-calon-2.html";
+        });
+      }
+      
+      var tombolDetailPaslon3 = document.getElementById("tombolDetailPaslon3");
+      if (tombolDetailPaslon3) {
+        tombolDetailPaslon3.addEventListener("click", function (e) {
+          window.location.href = "./detail-calon-3.html";
+        });
+      }
+
+      var tombolDetailPaslon3 = document.getElementById("tombolDetailPaslon21");
+      if (tombolDetailPaslon3) {
+        tombolDetailPaslon3.addEventListener("click", function (e) {
+          window.location.href = "./survey.html";
+        });
+      }
+
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer1");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer2");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer3");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer4");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer5");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
+      var tombolDetailPaslon1 = document.getElementById("newsItemsContainer6");
+      if (tombolDetailPaslon1) {
+        tombolDetailPaslon1.addEventListener("click", function (e) {
+          window.location.href = "./berita.html";
+        });
+      }
